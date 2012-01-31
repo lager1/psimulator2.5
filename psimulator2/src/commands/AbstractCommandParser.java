78a812a70213b033c035eca1205e3527358a13d8
tomas.pitrinec <tomas.pitrinec@gmail.com>
2012-01-31 09:08:41 +0000

    Ještě 	trochu kolem AbstractCommandParser

--- psimulator2/src/commands/AbstractCommandParser.java
+++ psimulator2/src/commands/AbstractCommandParser.java
@@ -6,8 +6,9 @@ package commands;
 /**
  * Abstraktni parser prikazu spolecnej pro linux i pro cisco.
  *
- * Parser si bude muset pamatovat posledni spusteny prikaz, aby kdyz dostane signal SIG_INT,
- * aby dokazal poslat prikaz vypnuti posledni spustene aplikace/prikazu.
+ * Parser si bude muset pamatovat posledni spusteny prikaz, aby kdyz dostane
+ * signal SIG_INT, aby dokazal poslat prikaz vypnuti posledni spustene
+ * aplikace/prikazu.
  *
  * @author neiss
  */
@@ -19,22 +20,32 @@ public abstract class AbstractCommandParser {
      * @param mode aktuali mod toho shellu (pro cisco)
      * @return navratovy kod
      */
-    public int processLine(String line, int mode){
+    public int processLine(String line, int mode) {
         return 0;
     }
+    
+    /**
+     * Slouzi k predavani uzivatelskyho vstupu prave spustenymu prikazu (kdyz se
+     * ten prikaz napr. na neco pta.
+     * @param userInput 
+     */
+    public abstract void catchUserInput(String userInput);
+    
+        /**
+     * Zavola parser se signalem od uzivatele. napr.: Ctrl+C, Ctrl+Z, ..
+     *
+     * @param sig
+     */
+    public abstract void catchSignal(int sig);
 
     /**
-     * Jednoducha metoda pro vypsani pouzitelnejch prikazu.
-     * Slouzi k jednoduchemu napovidani.
+     * Jednoducha metoda pro vypsani pouzitelnejch prikazu. Slouzi k
+     * jednoduchemu napovidani.
+     *
      * @param mode aktuali mod toho shellu (pro cisco)
      * @return
      */
-    public abstract String [] getCommands(int mode);
+    public abstract String[] getCommands(int mode);
 
-	/**
-	 * Zavola parser se signalem od uzivatele.
-	 * napr.: Ctrl+C, Ctrl+Z, ..
-	 * @param sig
-	 */
-	public abstract void catchSignal(int sig);
+
 }
