4cd0d017b81dbe3635ed9da816608d610b251843
marlukcz <marlukcz@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-04-15 13:18:15 +0000

    linux cat command implementation

--- psimulator2/src/commands/linux/filesystem/Cat.java
+++ psimulator2/src/commands/linux/filesystem/Cat.java
@@ -1,13 +1,16 @@
 /*
  * Erstellt am 12.4.2012.
  */
-
 package commands.linux.filesystem;
 
 import commands.AbstractCommandParser;
+import filesystem.dataStructures.jobs.InputFileJob;
+import java.io.InputStream;
+import java.util.Scanner;
 
 /**
  * Linux command cat. No options are supported.
+ *
  * @author Tomas Pitrinec
  */
 public class Cat extends FileSystemCommand {
@@ -26,11 +29,25 @@ public class Cat extends FileSystemCommand {
 	 */
 	@Override
 	protected void executeCommand() {
-		throw new UnsupportedOperationException("Not supported yet.");
+
+		for (String fileName : files) {
+			parser.device.getFilesystem().runInputFileJob(fileName, new InputFileJob() {
+
+				@Override
+				public int workOnFile(InputStream input) throws Exception {
+					Scanner sc = new Scanner(input);
+
+					while (sc.hasNextLine()) {
+						parser.getShell().printLine(sc.nextLine());
+					}
+
+					return 0;
+				}
+			});
+		}
+
+
+
+
 	}
-
-
-
-
-
 }
--- psimulator2/src/commands/linux/filesystem/FileSystemCommand.java
+++ psimulator2/src/commands/linux/filesystem/FileSystemCommand.java
@@ -10,7 +10,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import utils.Util;
 
 /**
  *
