09c90b14f781624e3e5a1b14b899b4626a8121d6
haldyr <haldyr@gmail.com>
2012-04-24 08:09:22 +0000

    small bugfix

--- psimulator2/src/commands/cisco/ShowCommand.java
+++ psimulator2/src/commands/cisco/ShowCommand.java
@@ -1,7 +1,6 @@
 /*
  * created 6.3.2012
  */
-
 package commands.cisco;
 
 import commands.AbstractCommandParser;
@@ -9,7 +8,6 @@ import commands.completer.Completer;
 import commands.completer.Node;
 import java.util.Iterator;
 import java.util.Map;
-import networkModule.L3.ArpCache;
 import networkModule.L3.ArpCache.ArpRecord;
 import networkModule.L3.ArpCache.Target;
 import networkModule.L3.CiscoIPLayer;
@@ -30,14 +28,13 @@ import utils.Util;
  */
 public class ShowCommand extends CiscoCommand {
 
-    private State showState = null;
-    private int ciscoState;
+	private State showState = null;
+	private int ciscoState;
 	private final CiscoIPLayer ipLayer;
-
-    /**
-     * Reference on interface in 'show interfaces FastEthernet0/0' command.
-     */
-    NetworkInterface iface;
+	/**
+	 * Reference on interface in 'show interfaces FastEthernet0/0' command.
+	 */
+	NetworkInterface iface;
 
 	public ShowCommand(AbstractCommandParser parser) {
 		super(parser);
@@ -75,7 +72,7 @@ public class ShowCommand extends CiscoCommand {
 		for (NetworkInterface ifa : ipLayer.getSortedNetworkIfaces()) {
 			if (ifa.name.matches("[a-zA-Z]+Ethernet[0-9]/[0-9]{1,2}")) {
 				int indexOfT = ifa.name.lastIndexOf("t");
-				String shortenedIfaceName = ifa.name.substring(0, indexOfT+1);
+				String shortenedIfaceName = ifa.name.substring(0, indexOfT + 1);
 				ifaces.addChild(new Node(shortenedIfaceName));
 				break;
 			}
@@ -86,116 +83,115 @@ public class ShowCommand extends CiscoCommand {
 		privileged.addCommand("show ip route");
 	}
 
-    enum State {
+	enum State {
 
 		ARP,
-        RUN,
-        ROUTE,
-        NAT,
-        QUESTION_MARK,
-        INTERFACES,
+		RUN,
+		ROUTE,
+		NAT,
+		QUESTION_MARK,
+		INTERFACES,
 		IP_INTERFACE,
-		IP_INTERFACE_BRIEF,
-    };
+		IP_INTERFACE_BRIEF,};
 
 	private void start() {
-        switch (showState) {
+		switch (showState) {
 			case ARP:
 				arp();
 				break;
-            case RUN:
-                runningConfig();
-                break;
-            case ROUTE:
-                ipRoute();
-                break;
-            case NAT:
-                ipNatTranslations();
-                break;
-            case INTERFACES:
-                interfaces();
-                break;
+			case RUN:
+				runningConfig();
+				break;
+			case ROUTE:
+				ipRoute();
+				break;
+			case NAT:
+				ipNatTranslations();
+				break;
+			case INTERFACES:
+				interfaces();
+				break;
 			case IP_INTERFACE:
 				ipInterface();
 				break;
 			case IP_INTERFACE_BRIEF:
 				ipInterfaceBrief();
 				break;
-        }
-    }
+		}
+	}
 
-    private boolean process() {
-        // show ip route
-        // show running-config      - jen v ROOT rezimu
-        // show interfaces          - jen v ROOT rezimu
-        // show ip nat translations
+	private boolean process() {
+		// show ip route
+		// show running-config      - jen v ROOT rezimu
+		// show interfaces          - jen v ROOT rezimu
+		// show ip nat translations
 		// show ip interface
 		// show ip interface brief
 		// show arp
 
-        String dalsi = nextWord(); // druhe slovo
-        if (dalsi.isEmpty()) {
-            printLine("% Type \"show ?\" for a list of subcommands\n");
-            return false;
-        }
+		String dalsi = nextWord(); // druhe slovo
+		if (dalsi.isEmpty()) {
+			printLine("% Type \"show ?\" for a list of subcommands\n");
+			return false;
+		}
 
-        if (dalsi.equals("?")) {
-            String s = "";
-            s += psimulator2.Psimulator.getNameOfProgram()+": in real cisco there is just first word for completion (not the whole command)\n\n";
-            s += "  show ip route                   IP routing table\n";
-            s += "  show ip nat translations        Translation entries\n";
-            if (ciscoState == CommandShell.CISCO_PRIVILEGED_MODE) {
-                s += "  show running-config             Current operating configuration\n";
-            }
-            s += "\n";
-            printWithDelay(s, 50);
-            return false;
-        }
+		if (dalsi.equals("?")) {
+			String s = "";
+			s += psimulator2.Psimulator.getNameOfProgram() + ": in real cisco there is just first word for completion (not the whole command)\n\n";
+			s += "  show ip route                   IP routing table\n";
+			s += "  show ip nat translations        Translation entries\n";
+			if (ciscoState == CommandShell.CISCO_PRIVILEGED_MODE) {
+				s += "  show running-config             Current operating configuration\n";
+			}
+			s += "\n";
+			printWithDelay(s, 50);
+			return false;
+		}
 
 		if (dalsi.startsWith("a") && ciscoState == CommandShell.CISCO_PRIVILEGED_MODE) {
 			if (!isCommand("arp", dalsi, 2)) {
-                return false;
-            }
+				return false;
+			}
 
 			showState = State.ARP;
 			return true;
 		}
 
-        if (dalsi.startsWith("r")) {
-            if (ciscoState == CommandShell.CISCO_USER_MODE) {
-                invalidInputDetected();
-                return false;
-            }
+		if (dalsi.startsWith("r")) {
+			if (ciscoState == CommandShell.CISCO_USER_MODE) {
+				invalidInputDetected();
+				return false;
+			}
 
-            if (!isCommand("running-config", dalsi, 3)) {
-                return false;
-            }
-            showState = State.RUN;
-            return true;
-        } else {
-            if (dalsi.startsWith("in")) {
-                if (ciscoState != CommandShell.CISCO_PRIVILEGED_MODE) {
-                    invalidInputDetected();
-                    return false;
-                }
-                if (!isCommand("interfaces", dalsi, 3)) {
-                    return false;
-                }
-                return processInterfaces();
-            }
+			if (!isCommand("running-config", dalsi, 3)) {
+				return false;
+			}
+			showState = State.RUN;
+			return true;
+		} else {
+			if (dalsi.startsWith("in")) {
+				if (ciscoState != CommandShell.CISCO_PRIVILEGED_MODE) {
+					invalidInputDetected();
+					return false;
+				}
+				if (!isCommand("interfaces", dalsi, 3)) {
+					return false;
+				}
+				return processInterfaces();
+			}
 
-            if (!isCommand("ip", dalsi, 2)) {
-                return false;
-            }
+			if (!isCommand("ip", dalsi, 2)) {
+				return false;
+			}
 
-            dalsi = nextWord();
-            if (dalsi.startsWith("r")) {
-                if (!isCommand("route", dalsi, 2)) {
-                    return false;
-                }
-                showState = State.ROUTE;
-                return true;
-            } else {
+			dalsi = nextWord();
+			if (dalsi.startsWith("r")) {
+				if (!isCommand("route", dalsi, 2)) {
+					return false;
+				}
+				showState = State.ROUTE;
+				return true;
+			} else {
 				if (dalsi.startsWith("i")) { // interface
 					if (!isCommand("interface", dalsi, 3)) {
 						return false;
@@ -226,28 +222,28 @@ public class ShowCommand extends CiscoCommand {
 					showState = State.NAT;
 					return true;
 				}
-            }
-        }
-    }
+			}
+		}
+	}
 
 	// show interfaces (FastEthernet 0/0)?
-    private boolean processInterfaces() {
-        showState = State.INTERFACES;
+	private boolean processInterfaces() {
+		showState = State.INTERFACES;
 
-        String rozh = nextWord();
-        if (rozh.isEmpty()) {
-            return true;
-        }
-        rozh += nextWord();
+		String rozh = nextWord();
+		if (rozh.isEmpty()) {
+			return true;
+		}
+		rozh += nextWord();
 
-        iface = null;
-        iface = ipLayer.getNetworkIntefaceIgnoreCase(rozh);
-        if (iface == null) {
+		iface = null;
+		iface = ipLayer.getNetworkIntefaceIgnoreCase(rozh);
+		if (iface == null) {
 			if (rozh.matches("[fF].*[0-9]/[0-9]{1,2}")) {
 				int end = rozh.indexOf("0");
 				String novy = "FastEthernet";
 				String cislo = rozh.substring(end, rozh.length());
-				rozh = novy+cislo;
+				rozh = novy + cislo;
 
 				iface = ipLayer.getNetworkIntefaceIgnoreCase(rozh);
 				if (iface != null) {
@@ -260,53 +256,54 @@ public class ShowCommand extends CiscoCommand {
 				return false;
 			}
 
-            invalidInputDetected();
-            return false;
-        }
-        return true;
-    }
+			invalidInputDetected();
+			return false;
+		}
+		return true;
+	}
 
-    /**
-     * Posle vypis pro prikaz 'show interfaces.
-     */
-    private void interfaces() {
-        if (iface == null) {
-            for (NetworkInterface nIface : ipLayer.getNetworkIfaces()) {
+	/**
+	 * Posle vypis pro prikaz 'show interfaces.
+	 */
+	private void interfaces() {
+		if (iface == null) {
+			for (NetworkInterface nIface : ipLayer.getNetworkIfaces()) {
 				printWithDelay(getInterfaceReport(nIface), 30);
-            }
-            printLine("");
-            return;
-        }
-        printWithDelay(getInterfaceReport(iface), 30);
-    }
+			}
+			printLine("");
+			return;
+		}
+		printWithDelay(getInterfaceReport(iface), 30);
+	}
 
 	/**
 	 * show arp
 	 */
 	private void arp() {
 		String s = "";
+		Target target;
 		Map<Target, ArpRecord> arp = ipLayer.getArpCache();
 		ipLayer.checkArpRecords();
-
 		s += "Protocol  Address          Age (sec)  Hardware Addr   Type   Interface\n";
 
-		Iterator<Target> it = arp.keySet().iterator();
-		Target target;
+		synchronized (arp) {
+			Iterator<Target> it = arp.keySet().iterator();
 
-		while (it.hasNext()) {
-			target = it.next();
+			while (it.hasNext()) {
+				target = it.next();
 
-			ArpRecord record = arp.get(target);
+				ArpRecord record = arp.get(target);
 
-			s += Util.zarovnej("Internet", 10);
-			s += Util.zarovnej(target.address.toString(), 17);
-			s += Util.zarovnej(""+(System.currentTimeMillis()-record.timeStamp)/1_000, 11);
-			s += Util.zarovnej(record.mac.getCiscoRepresentation(), 16);
-			s += Util.zarovnej("ARPA", 7);
-			s += target.iface.name;
+				s += Util.zarovnej("Internet", 10);
+				s += Util.zarovnej(target.address.toString(), 17);
+				s += Util.zarovnej("" + (System.currentTimeMillis() - record.timeStamp) / 1_000, 11);
+				s += Util.zarovnej(record.mac.getCiscoRepresentation(), 16);
+				s += Util.zarovnej("ARPA", 7);
+				s += target.iface.name;
 
-			if (it.hasNext()) {
-				s += "\n";
+				if (it.hasNext()) {
+					s += "\n";
+				}
 			}
 		}
 
@@ -331,7 +328,7 @@ public class ShowCommand extends CiscoCommand {
 			}
 
 			if (nIface.getIpAddress() != null) {
-				s += "  Internet address is " + nIface.getIpAddress().toString()+"\n";
+				s += "  Internet address is " + nIface.getIpAddress().toString() + "\n";
 				s += "  Broadcast address is 255.255.255.255\n";
 				s += "  Address determined by setup command\n";
 				s += "  MTU is 1500 bytes\n";
@@ -383,153 +380,152 @@ public class ShowCommand extends CiscoCommand {
 		printWithDelay(s, 50);
 	}
 
-    /**
-     * Posle vypis pro prikaz 'show ip nat translations.
-     */
-    private void ipNatTranslations() {
-        String s = "";
+	/**
+	 * Posle vypis pro prikaz 'show ip nat translations.
+	 */
+	private void ipNatTranslations() {
+		String s = "";
 
 		NatTable table = ipLayer.getNatTable();
 		table.deleteOldDynamicRecords();
 
 		if (table.getDynamicRules().isEmpty()) {
-            s += "\n\n";
+			s += "\n\n";
 			printWithDelay(s, 50);
-            return;
-        }
+			return;
+		}
 
 		s += Util.zarovnej("Pro Inside global", 24) + Util.zarovnej("Inside local", 20);
-        s += Util.zarovnej("Outside local", 20) + Util.zarovnej("Outside global", 20);
-        s += "\n";
+		s += Util.zarovnej("Outside local", 20) + Util.zarovnej("Outside global", 20);
+		s += "\n";
 
-        for (Record zaznam : table.getDynamicRules()) {
+		for (Record zaznam : table.getDynamicRules()) {
 			s += Util.zarovnej("icmp " + zaznam.out.getAddressWithPort(), 24)
 					+ Util.zarovnej(zaznam.in.getAddressWithPort(), 20)
 					+ Util.zarovnej(zaznam.target.toString(), 20)
-					+ Util.zarovnej(zaznam.target.toString(), 20)+"\n";
-        }
+					+ Util.zarovnej(zaznam.target.toString(), 20) + "\n";
+		}
 
-        for (NatTable.StaticRule rule : table.getStaticRules()) {
+		for (NatTable.StaticRule rule : table.getStaticRules()) {
 			s += Util.zarovnej("--- " + rule.out, 24)
 					+ Util.zarovnej(rule.in.toString(), 20)
 					+ Util.zarovnej("---", 20)
 					+ Util.zarovnej("---", 20) + "\n";
-        }
-
-		printWithDelay(s, 50);
-    }
-
-    /**
-     * Posle vypis pro prikaz 'show ip route'.
-     */
-    private void ipRoute() {
-        String s = "";
-        s += ipLayer.wrapper.getShIpRoute();
-        printWithDelay(s, 50);
-    }
-
-    /**
-     * Prikaz 'show running-config' ve stavu # (ROOT).
-     * Aneb vypis rozhrani v uplne silenem formatu.
-     */
-    private void runningConfig() {
-        String s = "";
-
-        s += "Building configuration...\n"
-                + "\n"
-                + "Current configuration : 827 bytes\n"
-                + "!\n"
-                + "version 12.4\n"
-                + "service timestamps debug datetime msec\n"
-                + "service timestamps log datetime msec\n"
-                + "no service password-encryption\n"
-                + "!\n"
-                + "hostname " + getDevice().getName() + "\n"
-                + "!\n";
-		if (!debug) {
-			s += "boot-start-marker\n"
-                + "boot-end-marker\n"
-                + "!\n"
-                + "!\n"
-                + "no aaa new-model\n"
-                + "!\n"
-                + "resource policy\n"
-                + "!\n"
-                + "mmi polling-interval 60\n"
-                + "no mmi auto-configure\n"
-                + "no mmi pvc\n"
-                + "mmi snmp-timeout 180\n"
-                + "ip subnet-zero\n"
-                + "ip cef\n"
-                + "!\n"
-                + "!\n"
-                + "no ip dhcp use vrf connected\n"
-                + "!\n"
-                + "!\n"
-                + "!\n";
 		}
 
-        for (NetworkInterface singleIface : ipLayer.getNetworkIfaces()) {
+		printWithDelay(s, 50);
+	}
 
-            s += "interface " + singleIface.name + "\n";
+	/**
+	 * Posle vypis pro prikaz 'show ip route'.
+	 */
+	private void ipRoute() {
+		String s = "";
+		s += ipLayer.wrapper.getShIpRoute();
+		printWithDelay(s, 50);
+	}
+
+	/**
+	 * Prikaz 'show running-config' ve stavu # (ROOT). Aneb vypis rozhrani v uplne silenem formatu.
+	 */
+	private void runningConfig() {
+		String s = "";
+
+		s += "Building configuration...\n"
+				+ "\n"
+				+ "Current configuration : 827 bytes\n"
+				+ "!\n"
+				+ "version 12.4\n"
+				+ "service timestamps debug datetime msec\n"
+				+ "service timestamps log datetime msec\n"
+				+ "no service password-encryption\n"
+				+ "!\n"
+				+ "hostname " + getDevice().getName() + "\n"
+				+ "!\n";
+		if (!debug) {
+			s += "boot-start-marker\n"
+					+ "boot-end-marker\n"
+					+ "!\n"
+					+ "!\n"
+					+ "no aaa new-model\n"
+					+ "!\n"
+					+ "resource policy\n"
+					+ "!\n"
+					+ "mmi polling-interval 60\n"
+					+ "no mmi auto-configure\n"
+					+ "no mmi pvc\n"
+					+ "mmi snmp-timeout 180\n"
+					+ "ip subnet-zero\n"
+					+ "ip cef\n"
+					+ "!\n"
+					+ "!\n"
+					+ "no ip dhcp use vrf connected\n"
+					+ "!\n"
+					+ "!\n"
+					+ "!\n";
+		}
+
+		for (NetworkInterface singleIface : ipLayer.getNetworkIfaces()) {
+
+			s += "interface " + singleIface.name + "\n";
 			if (singleIface.getIpAddress() == null) {
 				s += " no ip address\n";
 			} else {
 				s += " ip address " + singleIface.getIpAddress().getIp() + " " + singleIface.getIpAddress().getMask() + "\n";
 			}
 
-            if (ipLayer.getNatTable().getOutside() != null && ipLayer.getNatTable().getOutside().name.equals(singleIface.name)) {
+			if (ipLayer.getNatTable().getOutside() != null && ipLayer.getNatTable().getOutside().name.equals(singleIface.name)) {
 				s += " ip nat outside" + "\n";
-            }
+			}
 
 			if (ipLayer.getNatTable().getInside(singleIface.name) != null) {
 				s += " ip nat inside" + "\n";
 			}
 
-            if (singleIface.isUp == false) {
-                s += " shutdown" + "\n";
-            }
-            s += " duplex auto\n speed auto\n!\n";
-        }
+			if (singleIface.isUp == false) {
+				s += " shutdown" + "\n";
+			}
+			s += " duplex auto\n speed auto\n!\n";
+		}
 
-        if (ipLayer.routingTable.classless) {
-            s += "ip classless\n";
-        }
-        s += ipLayer.wrapper.getRunningConfig();
+		if (ipLayer.routingTable.classless) {
+			s += "ip classless\n";
+		}
+		s += ipLayer.wrapper.getRunningConfig();
 
-        s += "!\n";
-        s += "ip http server\n";
+		s += "!\n";
+		s += "ip http server\n";
 
-        for (Pool pool : ipLayer.getNatTable().lPool.getSortedPools()) {
-            s += "ip nat pool " + pool.name + " " + pool.getFirst() + " " + pool.getLast()
-                    + " prefix-length " + pool.prefix + "\n";
-        }
+		for (Pool pool : ipLayer.getNatTable().lPool.getSortedPools()) {
+			s += "ip nat pool " + pool.name + " " + pool.getFirst() + " " + pool.getLast()
+					+ " prefix-length " + pool.prefix + "\n";
+		}
 
-        for (PoolAccess pa : ipLayer.getNatTable().lPoolAccess.getSortedPoolAccess()) {
-            s += "ip nat inside source list " + pa.access + " pool " + pa.poolName;
-            if (pa.overload) {
-                s += " overload";
-            }
-            s += "\n";
-        }
+		for (PoolAccess pa : ipLayer.getNatTable().lPoolAccess.getSortedPoolAccess()) {
+			s += "ip nat inside source list " + pa.access + " pool " + pa.poolName;
+			if (pa.overload) {
+				s += " overload";
+			}
+			s += "\n";
+		}
 
 		for (StaticRule rule : ipLayer.getNatTable().getStaticRules()) {
 			s += "ip nat inside source static " + rule.in + " " + rule.out + "\n";
 		}
 
-        s += "!\n";
+		s += "!\n";
 
-        for (AccessList access : ipLayer.getNatTable().lAccess.getList()) {
-            s += "access-list " + access.number + " permit " + access.ip.getIp() + " " + access.ip.getMask().getWildcardRepresentation() + "\n";
-        }
+		for (AccessList access : ipLayer.getNatTable().lAccess.getList()) {
+			s += "access-list " + access.number + " permit " + access.ip.getIp() + " " + access.ip.getMask().getWildcardRepresentation() + "\n";
+		}
 
-        if (!debug) {
-            s += "!\n" + "!\n" + "control-plane\n"
-                    + "!\n" + "!\n" + "line con 0\n"
-                    + "line aux 0\n" + "line vty 0 4\n" + " login\n" + "!\n" + "end\n\n";
-        }
-        printWithDelay(s, 10);
-    }
+		if (!debug) {
+			s += "!\n" + "!\n" + "control-plane\n"
+					+ "!\n" + "!\n" + "line con 0\n"
+					+ "line aux 0\n" + "line vty 0 4\n" + " login\n" + "!\n" + "end\n\n";
+		}
+		printWithDelay(s, 10);
+	}
 
 	private String getInterfaceReport(NetworkInterface iface) {
 		String s = iface.name + " is ";
@@ -594,4 +590,3 @@ public class ShowCommand extends CiscoCommand {
 		return s;
 	}
 }
-
