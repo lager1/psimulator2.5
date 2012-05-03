2bf33da7a634f61a073e673fb0d520395f1a13de
marlukcz <marlukcz@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-05-03 10:44:21 +0000

    texteditor home/end key

--- psimulator2/src/telnetd/io/toolkit/myToolkit/MyEditLine.java
+++ psimulator2/src/telnetd/io/toolkit/myToolkit/MyEditLine.java
@@ -13,27 +13,26 @@ import telnetd.io.toolkit.Editline;
  *
  * @author Martin Lukáš <lukasma1@fit.cvut.cz>
  */
-public class MyEditLine extends Editline implements Quitable{
+public class MyEditLine extends Editline implements Quitable {
 
 	/**
 	 * reference to component redrawer
 	 */
 	private ComponentReDrawer componentReDrawer;
 	private boolean quit = false;
-	
+
 	public MyEditLine(BasicTerminalIO io) {
 		super(io);
 	}
 
 	/**
 	 *
-	 * @param componentReDrawer 
+	 * @param componentReDrawer
 	 */
 	public void setComponentReDrawer(ComponentReDrawer componentReDrawer) {
 		this.componentReDrawer = componentReDrawer;
 	}
-	
-	
+
 	@Override
 	public int run() throws IOException {
 		int in;
@@ -51,7 +50,7 @@ public class MyEditLine extends Editline implements Quitable{
 			m_LastCursPos = m_Cursor;
 
 			this.componentReDrawer.drawComponents();
-			
+
 			switch (in) {
 				case BasicTerminalIO.LEFT:
 					if (!moveLeft()) {
@@ -89,6 +88,17 @@ public class MyEditLine extends Editline implements Quitable{
 						Logger.log(Logger.WARNING, LoggingCategory.TELNET, "BufferOverflowException occured when running EditLine component");
 					}
 
+				}
+				break;
+				case TerminalIO.HOME_KEY: {
+					while (moveLeft()) {
+					}
+				}
+				break;
+
+				case TerminalIO.END_KEY: {
+					while (moveRight()) {
+					}
 				}
 				break;
 
