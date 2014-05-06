246a3eb089f133e378bfab884a9244ad7dd965af
Václav Mach <machvac5@fit.cvut.cz>
2014-05-06 02:57:07 +0000

    oprava chyby tlaticka ok ve vlastnotech zarizeni

--- commands/Rnetconn.java
+++ commands/Rnetconn.java
@@ -16,14 +16,13 @@ import utils.Util;
 /**
  * This command is used to manage connections from simulator to real network.
  * @author Tomas Pitrinec
+ * @author Václav Mach <machvac5 at fit.cvut.cz>
  */
 public class Rnetconn extends AbstractCommand {
 
 	public Rnetconn(AbstractCommandParser parser) {
 		super(parser);
 	}
-
-
 
 	@Override
 	public void run() {
